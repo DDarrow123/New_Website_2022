30e4376a358ff5217d55fc32d9c7060ca3c8fe6f
Devon <ddarrowphl@gmail.com>
2022-09-13 11:13:50 -0400

    finished welcome page layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import bridge from "../assets/images/daniel-verdugo-unsplash.jpg";
 
 const Home = () => {
     return (
         <div className='home'>
             <header className='home__intro'>Welcome</header>
-            <div className='home__name-wrapper'>
-                <span className='home__name'>DEVON DARROW</span>
-                <img src={bridge} alt='Williamsburg bridge' className='home__image'></img>
+            <div className='home__welcome-info'>
+                <div className='home__name-wrapper'>
+                    <span className='home__name'>Devon Darrow</span>
+                    <img src={bridge} alt='Williamsburg bridge - aka the best view of the city' className='home__image'></img>
+                </div>
+                <div className='home__tagline'>
+                    <span className='home__job-type'>Front-end web developer</span>
+                    <span className='home__job-location'>New York | Paris</span>
+                </div>
             </div>
-            <div className='home__tagline'><span className='home__job-type'>Front-end web developer</span></div>
         </div>
     )
 }
