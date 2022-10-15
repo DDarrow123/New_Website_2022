9eca794a9562beb4c4115cfb6f9acd162f80cd6c
Devon <ddarrowphl@gmail.com>
2022-10-15 00:00:16 -0400

    revamping work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import { TweenMax, Elastic, TweenLite, TimelineLite, CSSPlugin } from "gsap";
 import line_img from "../assets/images/line_img.jpg";
-import festival_img from "../assets/images/festival_img.png";
-import question_img from "../assets/images/question_img.png";
-import beauty_img from "../assets/images/beauty_img.png";
-import gallery_img from "../assets/images/gallery_img.png";
-import fashion_lister from "../assets/images/fashion_lister.png";
-import fashion_week from "../assets/images/fashion_week.png";
-import fright_matic from "../assets/images/fright_matic.png";
 import play_icon from "../assets/images/play_icon.svg";
 import restart_icon from "../assets/images/restart_icon.svg";
 import pause_icon from "../assets/images/pause_icon.svg";
@@ -33,25 +26,31 @@ class Work extends Component {
             <img
               className="site-btn"
               src={play_icon}
+              alt=""
               onClick={() => this.tl.play()}
             />
             <img
               className="site-btn"
               src={pause_icon}
+              alt=""
               onClick={() => this.tl.pause()}
             />
             <img
               className="site-btn"
               src={restart_icon}
+              alt=""
               onClick={() => this.tl.reverse()}
             />
           </div>
           <h3 className="name-title">
-            <img className="welcome-line-img" src={line_img} alt="line image" />
+            <img 
+              className="welcome-line-img" 
+              src={line_img} 
+              alt="" />
             Devon Darrow
           </h3>
         </header>
-        <div className="split-page-container">
+        <div className="work-container">
           <div className="left-work-bar">
             <div className="work-overlay">
               <div className="work-title">
@@ -67,81 +66,67 @@ class Work extends Component {
             </div>
           </div>
           <div className="right-work-bar">
-            <div className="implementation-title">
+            <div className="right-container">
               <div className="implementation-container">
+                  <div className="work-name">
+                    <span>1</span> Front-end work at Insider
+                    <div className="card card-background">
+                      <a
+                        href="https://www.businessinsider.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {/* <img
+                          className="work-logo" 
+                          src={ work_logo } 
+                          alt=""
+                        /> */}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="work-name">
+                    {" "}
+                    <span>2</span> Festival Site
+                    <div className="card">
+                      <a
+                        href="https://www.devondarrow.com/Example_2/index.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {/* <img 
+                          className="work-image" 
+                          src={ festival_img } 
+                          alt=""
+                        /> */}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="work-name">
+                    <span>3</span> Gallery Site
+                    <div className="card">
+                      <a
+                        href="https://www.devondarrow.com/Example_3"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        alt=""
+                      >
+                        {/* <img 
+                          src={ gallery_img }
+                          alt="" 
+                        /> */}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="project-title">
+                <div className="project-container">
                 <div className="work-name">
                   {" "}
-                  <span>1</span> Festival Site
-                  <div className="card">
-                    <a
-                      href="https://www.devondarrow.com/Example_2/index.html"
-                      target="_blank"
-                    >
-                      <img className="work-image" src={festival_img} />
-                    </a>
-                  </div>
+                  <span>...</span> More work coming soon
                 </div>
-                <div className="work-name">
-                  <span>2</span> Beauty Site
-                  <div className="card">
-                    <a
-                      href="https://www.devondarrow.com/Example_1/index.html"
-                      target="_blank"
-                    >
-                      <img src={beauty_img} />
-                    </a>
-                  </div>
                 </div>
-                <div className="work-name">
-                  <span>3</span> Gallery Site
-                  <div className="card">
-                    <a
-                      href="https://www.devondarrow.com/Example_3"
-                      target="_blank"
-                    >
-                      <img src={gallery_img} />
-                    </a>
                   </div>
-                </div>
               </div>
-              <div className="project-title">
-                <div className="project-container">
-                  <div className="work-name">
-                    <span>4</span> Fashion Lister
-                    <div className="card">
-                      <a
-                        href="https://github.com/DDarrow123/Fashion_Lister"
-                        target="_blank"
-                      >
-                        <img src={fashion_lister} />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="work-name">
-                    <span>5</span> Fashion Reviews
-                    <div className="card">
-                      <a
-                        href="https://github.com/DDarrow123/Mod-3-Final-Project-Front-End"
-                        target="_blank"
-                      >
-                        <img src={fashion_week} />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="work-name">
-                    <span>6</span> Fright-O-Matic
-                    <div className="card">
-                      <a
-                        href="https://github.com/free2580/Fright-O-Matic"
-                        target="_blank"
-                      >
-                        <img src={fright_matic} />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </div>  
           </div>
         </div>
       </div>
